0da81487abae571ea3fcd3fd92cfeee4c3ef154e
firekind <shyamant.achar@gmail.com>
2022-03-23 11:00:01 +0000

    updated main

--- mat-to-surf/main.cpp
+++ mat-to-surf/main.cpp
@@ -1,8 +1,31 @@
 #include "gst/gst.h"
 
+#include <signal.h>
+#include <unistd.h>
 #include <fstream>
+#include <atomic>
+#include <thread>
+
+static std::atomic<bool> running(true);
+
+void interruptHandler(int) {
+    running.store(false);
+}
+
+void start_pipeline(GMainContext* context, GMainLoop* loop, GstElement* pipeline) {
+    g_main_context_push_thread_default(context);
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    g_main_loop_run(loop);
+}
 
 int main() {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = interruptHandler;
+    sigfillset(&sa.sa_mask);
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+
     gst_init(nullptr, nullptr);
     GstRegistry* registry = gst_registry_get();
     gst_registry_scan_path(registry, "mat-to-surf/gst");
@@ -14,11 +37,14 @@ int main() {
 
     GMainContext* context = g_main_context_new();
     GMainLoop* loop = g_main_loop_new(context, false);
-    g_main_context_push_thread_default(context);
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    g_main_loop_run(loop);
+    std::thread thread(start_pipeline, context, loop, pipeline);
+
+    while (running.load()) {}
 
     gst_element_set_state(pipeline, GST_STATE_NULL);
     g_main_loop_quit(loop);
+    thread.join();
+
     gst_object_unref(pipeline);
+    printf("done\n");
 }
